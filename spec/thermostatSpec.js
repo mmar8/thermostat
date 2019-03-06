d20c05ae90a68574473c2e99fe96dec36e6e3a88
James Ault <james.ault@johnlewis.co.uk>
2019-03-06 15:22:41 +0000

    Passing test for powersave mode on

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,4 +1,8 @@
+'use strict';
+
 describe('Thermostat', function() {
+    var thermostat;
+    var i;
     it('starts at 20 degrees', function(){
         thermostat = new Thermostat();
         expect(thermostat.temperature).toEqual(20);
@@ -10,10 +14,27 @@ describe('Thermostat', function() {
         expect(thermostat.temperature).toEqual(21);
     });
 
-    it('can decrese the temerature with a down function', function(){
+    it('can decrease the temerature with a down function', function(){
         thermostat = new Thermostat()
         thermostat.down()
         expect(thermostat.temperature).toEqual(19);
+    });
+
+    it('can not decrease the temerature below 10', function(){
+        thermostat = new Thermostat()
+        for (i = 0; i<=11; i++) {
+            thermostat.down()
+        }
+        expect(thermostat.temperature).toEqual(10);
+    });
+
+    it('can not increase the temperature above 25 if power saving mode is on', function(){
+        thermostat = new Thermostat()
+        thermostat.powersave('on')
+        for (i = 0; i<=6; i++) {
+            thermostat.up()
+        }
+        expect(thermostat.temperature).toEqual(25);
     });
 
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,14 +1,30 @@
 function Thermostat() {
     this.temperature = 20;
+    this.minimumTemperature = 10
+    this.powersavemode = true
 
 };
 
 Thermostat.prototype.up = function() {
-    this.temperature++
+    if (this.powersavemode === true && this.temperature < 25) {
+    this.temperature++}
+    else {return this.temperature}
     console.log(this.temperature)
 }
 
 Thermostat.prototype.down = function() {
-    this.temperature--
+    if (this.temperature > this.minimumTemperature) {
+    this.temperature--} else
+    { return this.temperature }
     console.log(this.temperature)
+}
+
+Thermostat.prototype.powersave = function(mode) {
+    if (mode === 'on') {
+        this.powersavemode = true
+        if (this.temperature > 25) { this.temperature = 25}
+    }
+    else if (mode === 'off') {
+        this.powersavemode = false
+    }
 }
