4116e4c79a1060f9677ef2b13b06c038c19d1b2b
Maria Vanaale <mvanaale@gmail.com>
2019-03-08 16:54:36 +0000

    css

--- src/interface.js
+++ src/interface.js
@@ -41,4 +41,19 @@ $(document).ready(function() {
        }  
      };
 
+     $('#current-city').change(function() {
+          var city = $('#current-city').val();
+          $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+            $('#current-temperature').text(data.main.temp)
+          })
+        })
+
+     function displayWeather(city) {
+          var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+          var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+          var units = '&units=metric';
+          $.get(url + token + units, function(data) {
+            $('#current-temperature').text(data.main.temp);
+          })
+     };
 });
